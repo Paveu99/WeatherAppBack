a1eeae3b12931648a5a78094f97bd6038ea8ef7b
Pawel Jarecki <paweljarecki10@gmail.com>
2023-07-27 18:20:16 +0200

    Creating types for cities

--- routers/weather.ts
+++ routers/weather.ts
@@ -1,34 +1,19 @@
 import {Router} from "express";
 import {getData} from "../utils/db";
-
-interface City {
-    "id_stacji": string,
-    "stacja": string,
-    "data_pomiaru": string,
-    "godzina_pomiaru": string,
-    "temperatura": string,
-    "predkosc_wiatru": string,
-    "kierunek_wiatru": string,
-    "wilgotnosc_wzgledna": string,
-    "suma_opadu": string,
-    "cisnienie": string
-}
+import {CheckCityReq} from "../types";
 
 export const weatherRouter = Router()
 
 weatherRouter
     .get('/', async (req, res) => {
         const weatherInPoland = await getData()
-        for (const i of weatherInPoland) {
-            console.log(i.stacja + ' ' + i.temperatura)
-        }
         res.json({
             weatherInPoland
         })
     })
     .get('/:id', async (req, res) => {
         const weatherInPoland = await getData()
-        let oneCity = weatherInPoland.filter((item: City) => item.stacja === req.params.id)[0]
+        let oneCity = weatherInPoland.filter((item: CheckCityReq) => item.stacja === req.params.id)[0]
 
         res.json({
             oneCity
