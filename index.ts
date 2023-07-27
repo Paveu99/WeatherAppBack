92bf01d5cf3584cbb9ed89c9a03de9b92047ae5d
Pawel Jarecki <paweljarecki10@gmail.com>
2023-07-27 17:52:56 +0200

    Created main router and errors validation handling

--- index.ts
+++ index.ts
@@ -3,13 +3,21 @@ import cors from "cors"
 import "express-async-errors"
 import {getData} from "./utils/db";
 import {weatherRouter} from "./routers/weather";
+import {main} from "./routers/main";
+import {handleError} from "./utils/errors";
 
 const app = express();
+
 app.use(cors({
     origin: 'http://localhost:3000'
 }));
+
 app.use(json());
-app.use('/weather', weatherRouter)
+
+app.use('/', main);
+app.use('/weather', weatherRouter);
+
+app.use(handleError);
 app.listen(3001, '0.0.0.0', () => {
     console.log('Listening on port http://localhost:3001')
-})
+});
