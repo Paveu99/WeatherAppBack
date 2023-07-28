83c3b95380e75289ab52607fed421b0610a13ca2
Pawel Jarecki <paweljarecki10@gmail.com>
2023-07-28 12:21:48 +0200

    Correction in item type: changed to CityEntity

--- routers/weather.ts
+++ routers/weather.ts
@@ -1,6 +1,6 @@
 import {Router} from "express";
 import {getData} from "../utils/db";
-import {CheckCityReq} from "../types";
+import {CityEntity} from "../types";
 
 export const weatherRouter = Router()
 
@@ -13,7 +13,7 @@ weatherRouter
     })
     .get('/:id', async (req, res) => {
         const weatherInPoland = await getData()
-        let oneCity = weatherInPoland.filter((item: CheckCityReq) => item.stacja === req.params.id)[0]
+        let oneCity = weatherInPoland.filter((item: CityEntity) => item.stacja === req.params.id)[0]
 
         res.json({
             oneCity
