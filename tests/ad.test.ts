3e817413806dc1d23822f815438cbee2a2a60095
Pawel Jarecki <paweljarecki10@gmail.com>
2023-07-29 12:53:06 +0200

    Added more tests to ad.test.ts

--- tests/ad.test.ts
+++ tests/ad.test.ts
@@ -8,3 +8,38 @@ test('Testing if choosing one city is working', async () => {
 
     expect(oneCity.stacja).toEqual('Białystok')
 })
+
+test('Testing whether the data that comes in is string', async () => {
+    const id: string = 'Białystok'
+    const weatherInPoland = await getData()
+    let oneCity = weatherInPoland.filter((item: CityEntity) => item.stacja === id)[0]
+
+    for (const oneCityKey in oneCity) {
+        expect(typeof oneCity[oneCityKey]).toBe('string')
+    }
+})
+
+test('Testing if the amount of cities is equal to 62', async () => {
+    const weatherInPoland = await getData()
+    expect(weatherInPoland.length).toEqual(62)
+})
+
+test('Testing if downloaded data is coming in', async () => {
+    const weatherInPoland = await getData()
+
+    expect(weatherInPoland).toBeTruthy()
+})
+
+test('Testing if number of keys in entity is equal 10', async () => {
+    const id: string = 'Białystok'
+    const weatherInPoland = await getData()
+    let oneCity = weatherInPoland.filter((item: CityEntity) => item.stacja === id)[0]
+
+    let numberOfKeys = 0
+
+    for (const oneCityKeys in oneCity) {
+        numberOfKeys++
+    }
+
+    expect(numberOfKeys).toEqual(10)
+})
